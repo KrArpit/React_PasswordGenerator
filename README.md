e548e1b1795ab854e889e605ad96cb0366542d57
KrArpit <arpit99kumar@gmail.com>
2024-08-21 11:41:59 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Password Generator App
+
+This is a simple password generator app that I built using React and Tailwind CSS. This project helped me learn about the basics of React, including:
+
+- How to use React hooks like `useState, useCallback, useEffect & useRef`
+- Writing JSX files
+- Styling components with Tailwind CSS
+- Understanding the basics of React component structure
+
+## About the Project
+
+This is my first project using React, and it focuses on generating random passwords based on user-selected criteria. The app is simple, but it was a great way for me to get hands-on experience with React and Tailwind.
+
---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(0);
+  const [number, setNumber] = useState(false);
+  const [specialChar, setSpecialChar] = useState(false);
+  const [password, setPassword] = useState("");
+  const [copy, setCopy] = useState("Copy");
+
+  const passwordRef = useRef(null); // useRef used to get reference of current element
+
+  // useCallback hook
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(number) str += "0123456789";
+    if(specialChar) str += "!#$%&()*+-/<=>?@[]^_{|}~";
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length +1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  },[number, specialChar, length, setPassword])
+
+  // function called, when you click on copy text
+  function copyText(){
+    window,navigator.clipboard.writeText(password);
+    if(password){
+      passwordRef.current.select();
+      setCopy("Copied");
+    setTimeout(()=>{
+      setCopy("Copy");
+    },1000);
+    }
+  }
+  // useEffect hook
+  useEffect(()=>{
+    passwordGenerator();
+  }, [number,specialChar,length, passwordGenerator]);
+
+  return (
+    <div className=' w-full h-screen flex items-center justify-center bg-[#0E0D12]'>
+      <div className='w-2/5'>
+      <h2 className='font-bold text-stone-400 text-center mb-2'>Password Generator</h2>
+      <div className='bg-[#23222B] flex justify-between items-center rounded-lg mb-3'>
+        <input
+        value={password}
+        ref={passwordRef}
+        className='bg-transparent px-3 py-4 outline-none text-3xl basis-full font-semibold text-slate-200 cursor-default'
+        type="text" 
+        placeholder='Pa$5W0rD!' 
+        readOnly />
+        <span 
+        onClick={copyText}
+        className='px-4 py-4 text-xl text-[#A3FFAE] font-medium cursor-pointer'>{copy}</span>
+        </div>
+        {/* 2nd Section */}
+        <div className='bg-[#23222B] rounded-lg p-5 relative'>
+          <div className='flex items-center justify-between mb-4' >
+          <label htmlFor="slider" className='flex flex-col basis-full text-lg text-slate-400'>Password Length
+            <input 
+            value={length}
+            onChange={(e)=>setLength(e.target.value)}
+            type="range" 
+            min={0} 
+            max={20} 
+            id='slider' 
+            className='accent-[#A3FFAE] cursor-pointer'/>
+          </label>
+          <span className='absolute right-5 top-4 text-2xl text-[#A3FFAE] font-semibold'>{length}</span>
+          </div>
+          {/* Checkbox */}
+          <div className='flex flex-col text-xl font-semibold text-slate-200 relative'>
+            <label className='flex items-center' htmlFor="number">
+              <input 
+              defaultChecked={number}
+              onChange={()=>{
+                setNumber(prev => !prev);
+              }}
+              className='w-5 h-5 mr-2 appearance-none border-2 hover:border-[#A3FFAE] checked:bg-[#A3FFAE] checked:border-[#A3FFAE] cursor-pointer' 
+              type="checkbox" id='number'/> 
+              {/* Condition, if number is checked then add tick icon */}
+              {number && <i className="fa-solid fa-check absolute text-stone-950 font-semibold left-[1.5px]"></i>}
+              Include Numbers</label>
+            <label className='flex items-center' htmlFor="special">
+              <input 
+              defaultChecked={specialChar}
+              onChange={()=>{
+                setSpecialChar(prev => !prev);
+              }}
+              className='w-5 h-5 mr-2 appearance-none border-2 hover:border-[#A3FFAE] checked:bg-[#A3FFAE] checked:border-[#A3FFAE] cursor-pointer' 
+              type="checkbox" id='special' />
+              {/* Condition, if specialChar is checked then add tick icon */}
+              {specialChar && <i className="fa-solid fa-check absolute text-stone-950 font-semibold left-[1.5px]"></i>}
+               Include Special Character</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
